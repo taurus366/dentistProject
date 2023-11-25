bb3bf0466610373343d22b0ac7e33724e9a98f0e
Arcerr <taurus..ali47>
2023-11-25 16:24:25 +0200

    .

--- HomeParking/src/main/java/com/github/taurus366/views/client/IndexPageView.java
+++ HomeParking/src/main/java/com/github/taurus366/views/client/IndexPageView.java
@@ -4,10 +4,11 @@ import com.github.taurus366.security.AuthenticatedUser;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.datetimepicker.DateTimePicker;
 import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.timepicker.TimePicker;
 import com.vaadin.flow.component.timepicker.TimePickerVariant;
@@ -16,27 +17,33 @@ import com.vaadin.flow.server.auth.AnonymousAllowed;
 import org.vaadin.addons.minicalendar.MiniCalendar;
 
 import java.time.*;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @AnonymousAllowed
 @PageTitle("Home")
 @Route(value = "home")
 @RouteAlias(value = "")
-public class IndexPageView extends VerticalLayout implements BeforeEnterObserver {
+public class IndexPageView extends FlexLayout implements BeforeEnterObserver {
     private final AuthenticatedUser authenticatedUser;
     public IndexPageView(AuthenticatedUser authenticatedUser) {
         this.authenticatedUser = authenticatedUser;
 
 
-        Button buttonBook = new Button("BOOK NOW");
+        Button buttonBook = new Button("РЕЗЕРВИРАЙТЕ СЕГА");
         buttonBook.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
+
+        /// DIALOG FOR SELECT DATE AND TIME
         Dialog dialogSelectDateTime = new Dialog();
-        dialogSelectDateTime.setHeaderTitle("Please select Date and Time");
+
+        VerticalLayout verticalLayout1 = new VerticalLayout();
+        H3 titleDialog = new H3("Моля, изберете дата и час");
+        titleDialog.getStyle().set("margin", "auto");
+        verticalLayout1.add(titleDialog);
+        dialogSelectDateTime.getHeader().add(verticalLayout1);
         VerticalLayout verticalLayout = new VerticalLayout();
         dialogSelectDateTime.add(verticalLayout);
+        dialogSelectDateTime.setHeight("465px");
 
 
         Button cancelBtn = new Button("cancel");
@@ -71,8 +78,6 @@ public class IndexPageView extends VerticalLayout implements BeforeEnterObserver
 
 
         });
-        timePicker.getElement().getPropertyNames().forEach(System.out::println);
-
 
         dialogSelectDateTime.add(calendar, timePicker);
 
@@ -82,7 +87,7 @@ public class IndexPageView extends VerticalLayout implements BeforeEnterObserver
             dialogSelectDateTime.open();
         });
 
-        add(buttonBook, dialogSelectDateTime);
+//        add(buttonBook, dialogSelectDateTime);
 
 //        Button button = new Button("CLICK ME TO REDIREC TO LOGIN");
 //
@@ -95,6 +100,102 @@ public class IndexPageView extends VerticalLayout implements BeforeEnterObserver
 //        });
 //
 //        add(button);
+
+
+
+        /// DESIGN FOR PAGE
+
+        // HEADER
+
+        Button btnLogin = new Button("Влез");
+        btnLogin.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        btnLogin.addClickListener(event -> getUI().ifPresent(ui -> ui.navigate("login")));
+
+        Button btnBook2 = new Button("Резервирай час");
+        btnBook2.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        btnBook2.addClickListener(event -> dialogSelectDateTime.open());
+        btnBook2.getStyle().set("margin-left", "10px");
+
+        Image imageIcon = new Image("images/logo.png", "logo");
+        imageIcon.getStyle()
+                .set("filter", "brightness(999) invert(0.5) sepia(1) hue-rotate(111deg)")
+                .set("width", "205px")
+                .set("height", "122px");
+
+        UnorderedList ul = new UnorderedList();
+//        ul.getStyle().set("margin", "5px");
+        ul.add(btnLogin);
+        ul.add(btnBook2);
+
+
+
+        Section sectionHeader = new Section();
+        sectionHeader.addClassName("header-sectionMain");
+        sectionHeader.getStyle()
+                        .set("display", "flex")
+                        .set("align-items", "center")
+                        .set("width", "96%")
+                        .set("justify-content", "space-between");
+        sectionHeader.add(imageIcon, ul);
+
+
+
+        // MAIN
+        Image image = new Image("images/female-dentist.png", "female dentist");
+            image.setWidth("100%");
+            image.getStyle().set("height", "150%")
+                    .set("object-fit", "cover")
+                    .set("box-shadow", "11px 7px 20px 0px #8d8d8d");
+
+
+           Paragraph paragraph = new Paragraph("Зъболекар с висок професионализъм и отношение към пациентите.");
+           paragraph.getStyle()
+                   .set("font-size", "45px")
+                   .set("line-height", "normal")
+                   .set("color", "#BB8150")
+                   .set("font-weight", "bold")
+                   .set("font-family", "Overpass");
+           Paragraph paragraph1 = new Paragraph("Изберете най-добрите специалисти с висок професионализъм и топло отношение, където никога няма място за осъждане.");
+           paragraph1.getStyle()
+                   .set("font-size", "20px")
+                   .set("font-family", "Overpass");
+
+
+           Article articleText = new Article();
+           articleText.setWidth("70%");
+           articleText.getStyle().set("padding-right", "30px");
+        articleText.add(paragraph, paragraph1, buttonBook);
+        articleText.getStyle().set("align-self", "self-end");
+
+           Article articleImg = new Article();
+//        articleImg.setWidth("100%");
+           articleImg.add(image);
+
+           Section sectionMain = new Section();
+           sectionMain.addClassName("main-sectionMain");
+           sectionMain.getStyle()
+                   .set("display", "flex")
+                   .set("margin", "0 30px 0 30px")
+                   .set("flex-wrap", "nowrap");
+//                   .set("align-items", "center");
+           sectionMain.add(articleText, articleImg);
+
+        Div div = new Div(sectionHeader, sectionMain);
+                    div.getStyle()
+                        .set("max-width", "1440px")
+                        .set("margin", "auto");
+        div.setWidth("100%");
+        div.setClassName("index-container");
+
+
+
+
+        add(div);
+
+
+
+
+
 
     }
 
