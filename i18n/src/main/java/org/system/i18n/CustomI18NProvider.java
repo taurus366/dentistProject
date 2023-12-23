df121ae85cd013d5697b86912a96d29411548e2e
Arcerr <taurus..ali47>
2023-12-23 11:41:00 +0200

    .

--- i18n/src/main/java/org/system/i18n/CustomI18NProvider.java
+++ i18n/src/main/java/org/system/i18n/CustomI18NProvider.java
@@ -1,19 +1,32 @@
 package org.system.i18n;
 
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.html.Section;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.i18n.I18NProvider;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.system.i18n.model.dto.LanguageDTO;
+import org.system.i18n.model.dto.MultiLangualTextAreaDTO;
+import org.system.i18n.model.entity.LanguageCustomerEntity;
+import org.system.i18n.service.LanguageCustomerService;
 import org.system.i18n.service.LanguageService;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.button.Button;
 
 import java.text.MessageFormat;
 import java.util.*;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 public class CustomI18NProvider implements I18NProvider {
 
     private final LanguageService languageService;
+    private final LanguageCustomerService languageCustomerService;
 
     public static final String BUNDLE_PREFIX = "i18n/translate";
 
@@ -22,8 +35,11 @@ public class CustomI18NProvider implements I18NProvider {
 
     private final List<Locale> locales = List.of(LOCALE_EN, LOCALE_BG);
 
-    public CustomI18NProvider(LanguageService languageService) {
+    private Map<Integer, String> data;
+
+    public CustomI18NProvider(LanguageService languageService, LanguageCustomerService languageCustomerService) {
         this.languageService = languageService;
+        this.languageCustomerService = languageCustomerService;
     }
 
     @Override
@@ -59,52 +75,140 @@ public class CustomI18NProvider implements I18NProvider {
 
     }
 
-    public ComboBox<LanguageDTO> getLanguageSelectorBox(String locale, String title) {
-        ComboBox<LanguageDTO> languageSelector = new ComboBox<>(title);
+    public ComboBox<LanguageDTO> getLanguageSelectorBox(String locale, String title, boolean isFlag) {
+        ComboBox<LanguageDTO> languageSelector = new ComboBox<>();
+
+        if(title != null) languageSelector.setLabel(title);
 
         final Collection<LanguageDTO> allActiveLanguages = this.getAllActiveLanguages();
 
         languageSelector.setItems(allActiveLanguages);
         languageSelector.setItemLabelGenerator(LanguageDTO::getName);
-//        languageSelector.setItems(LOCALE_BG, LOCALE_EN);
-//            languageSelector.setItems(allActiveLanguages
-//                    .stream()
-//                    .map(LanguageDTO::getName)
-//                    .collect(Collectors.toList()));
-//                languageSelector.setItems(Map.of("a", "1"));
+        if(isFlag)
+            languageSelector.setRenderer(new ComponentRenderer<>(language -> {
+                CountryFlagT flag = new CountryFlagT(language.getCode(), false);
+                flag.getStyle().set("width", "30px").set("height", "29px");
 
+                Span label = new Span(language.getName());
+
+                // Combine the flag and label in a HorizontalLayout
+                HorizontalLayout layout = new HorizontalLayout(flag, label);
+                layout.setAlignItems(FlexComponent.Alignment.CENTER); // Adjust alignment if needed
+
+                return layout;
+            }));
 
         final LanguageDTO languageByLocale = languageService.getLanguageByLocale(locale);
 
         languageSelector.setValue(languageByLocale);
 
-//        languageSelector.setValue(VaadinService.getCurrentRequest().getLocale());
-
-//        languageSelector.addValueChangeListener(event -> {
-//            System.out.println(event);
-////            UI.getCurrent().setDirection(Direction.LEFT_TO_RIGHT);
-//////            Locale selectedLocale = event.getValue();
-////            UI.getCurrent().setLocale(LOCALE_BG);
-//
-//            UI.getCurrent().getPage().reload();
-//        });
-//        https://github.com/vaadin/vaadin-localization-example
         return languageSelector;
     }
 
-    public List<LanguageDTO> getAllLanguages() {
-        return this.languageService.getAllLanguages();
+    public ComboBox<LanguageDTO> getLanguageSelectorBoxCustomer(String locale, String title, boolean isFlag) {
+
+        ComboBox<LanguageDTO> languageSelector = new ComboBox<>();
+
+        if(title != null) languageSelector.setLabel(title);
+
+
+        final Collection<LanguageDTO> allActiveLanguages = this.getAllActiveLanguages();
+
+        languageSelector.setItems(allActiveLanguages);
+        languageSelector.setItemLabelGenerator(LanguageDTO::getName);
+        if(isFlag)
+        languageSelector.setRenderer(new ComponentRenderer<>(language -> {
+            CountryFlagT flag = new CountryFlagT(language.getCode(), false);
+            flag.getStyle().set("width", "30px").set("height", "29px");
+
+            Span label = new Span(language.getName());
+
+            // Combine the flag and label in a HorizontalLayout
+            HorizontalLayout layout = new HorizontalLayout(flag, label);
+            layout.setAlignItems(FlexComponent.Alignment.CENTER); // Adjust alignment if needed
+
+            return layout;
+        }));
+
+        final LanguageDTO languageById = languageService.getLanguageByLocale(locale);
+
+        languageSelector.setValue(languageById);
+
+        return languageSelector;
     }
 
     public List<LanguageDTO> getAllActiveLanguages() {
         return this.languageService.getAllActiveLanguages();
     }
-
-    public LanguageDTO getLanguageById(Long langId) {
-        return this.languageService.getLanguageById(langId);
-    }
-
-    public LanguageDTO getLanguageByLocale(String locale) {
-        return this.languageService.getLanguageByLocale(locale);
-    }
+//    public MultiLangualTextAreaDTO multiLanguageTextArea(Map<Integer, String> data) {
+//        CustomI18NProvider provider = new CustomI18NProvider(languageService, languageCustomerService);
+//        final List<LanguageDTO> allActiveLanguages = provider.getAllActiveLanguages();
+//        AtomicInteger currentLanguageIndex = new AtomicInteger(1);
+//        this.data = data;
+//
+//        HorizontalLayout layout = new HorizontalLayout();
+//        layout.setSpacing(false);
+//        layout.getStyle()
+//                .set("display", "flex")
+//                .set("flex-direction", "column")
+////                .set("align-items", "center")
+//                .set("width", "100%");
+//
+//        TextArea textArea = new TextArea();
+//        textArea.getStyle()
+//                .set("width", "100%")
+//                .set("max-height", "58px")
+//                .set("min-height", "58px");
+//
+//        Section sectionTextAreaAndFlag = new Section();
+//        sectionTextAreaAndFlag.getStyle()
+//                .set("display", "flex")
+//                .set("width", "100%")
+//                .set("align-items", "center")
+//                .set("justify-content", "space-around");
+//
+//
+//        CountryFlagT flag = new CountryFlagT(allActiveLanguages.get(currentLanguageIndex.get()).getCode(), false);
+//
+//        Integer selectedLanguageId1 = Integer.parseInt(String.valueOf(allActiveLanguages.get(currentLanguageIndex.get()).getId()));
+//        String dataText1 = this.data.getOrDefault(selectedLanguageId1, ""); // Get the text associated with the selected language ID
+//
+//        textArea.setValue(dataText1);
+//
+//        flag.addClickListener(event -> {
+//
+//            if(currentLanguageIndex.get() + 1 < allActiveLanguages.size()){
+//                flag.setCountry(allActiveLanguages.get(currentLanguageIndex.addAndGet(1)).getCode());
+//            } else {
+//                currentLanguageIndex.set(0);
+//                flag.setCountry(allActiveLanguages.get(currentLanguageIndex.get()).getCode());
+//            }
+//
+//            Integer selectedLanguageId = Integer.parseInt(String.valueOf(allActiveLanguages.get(currentLanguageIndex.get()).getId()));
+//            String dataText = this.data.getOrDefault(selectedLanguageId, ""); // Get the text associated with the selected language ID
+//
+//            textArea.setValue(dataText);
+//        });
+//        textArea.addValueChangeListener(event -> {
+//            Integer selectedLanguageId = Integer.parseInt(String.valueOf(allActiveLanguages.get(currentLanguageIndex.get()).getId()));
+//            this.data.put(selectedLanguageId, textArea.getValue());
+//
+//        });
+//
+//        Button btnSave = new Button("Save");
+//
+//
+//        sectionTextAreaAndFlag.add(textArea, flag);
+//
+//        layout.add(sectionTextAreaAndFlag, btnSave);
+//
+//        MultiLangualTextAreaDTO dto = new MultiLangualTextAreaDTO();
+//        dto.setTextArea(textArea);
+//        dto.setData(this.data);
+//        dto.setButton(btnSave);
+//        dto.setHorizontalLayout(layout);
+//        dto.setFlag(flag);
+//
+//        return dto;
+//    }
 }
